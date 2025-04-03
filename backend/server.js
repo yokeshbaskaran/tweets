f8954a824afa4ce47035e6538dcdb1ed34e1b9d7
Yokesh <yokeshbaskaran@gmail.com>
2025-04-03 16:46:32 +0530

    render link updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,10 @@ app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://tweets-of-messages.onrender.com",
+    ],
     credentials: true,
   })
 );
@@ -31,10 +34,22 @@ mongoose
     console.log("DB connection lost" + err.message);
   });
 
+//routes
+
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+// --------------------Deployment---------------
+
+app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+});
+
+//--------------------Deployment---------------
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
